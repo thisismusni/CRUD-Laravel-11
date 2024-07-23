131602887904ae4f681c7266ec3800cd899d6d8b
thisismusni <56000665+thisismusni@users.noreply.github.com>
2024-07-23 21:57:03 +0800

    make controller and definition routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\BarangController;
+use App\Models\Barang;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('barang', BarangController::class);
